a5220598e066b70037e8612c624649ebe4a820ef
Felix <juan@baum.digital>
2016-12-01 13:44:01 -0600

    HttpServices sends to response interceptor

--- HttpService.js
+++ HttpService.js
@@ -1,9 +1,7 @@
 import httpUtil from './src/http/util';
 import {HttpException, NetworkException} from './exceptions/HttpException';
 import {ERROR_MESSAGES} from './src/http/constants';
-
 var _interceptors = [];
-
 export default
 class http 
 {
@@ -12,56 +10,44 @@ class http
 		try{
 			let data = runRequestInterceptorsStack(url, payload, options);
 			var response = await httpUtil.request(data.url, options.method, payload, options.headers);
-
 			
-
 		}catch(err){
 			if(err.name == 'InterceptorRejected') return err.payload;
+			runResponseInterceptorsStack({}, {status: -1}, options);
 			throw new NetworkException(err.message, payload);
 		}
 		
 		try{
 			var payload = await response.json();
 		}catch(err){}
-
 		let result = runResponseInterceptorsStack(payload, response, options);
 		if(String(response.status).charAt(0) != 2) throw new HttpException( ERROR_MESSAGES.HTTP_ERROR, response.status, payload, payload );
 		
 		return result;
 	}
-
-
 	static pushInterceptor(interceptor){
 		_interceptors.push(interceptor);
 	}
 }
-
-
 function runRequestInterceptorsStack(url, payload, options)
 {
 	//Copy array
 	let interceptors = _interceptors.slice();
 	if(options.interceptors) interceptors = interceptors.concat(options.interceptors);
-
 	interceptors.forEach((interceptorObj, index) => {
 		let interceptor = new interceptorObj({url, payload, options});
 		return interceptor.request(url, payload, options);
 	});
-
 	return {url, payload, options};
 }
-
-
 function runResponseInterceptorsStack(response, raw, options)
 {
 	//Copy array
 	let interceptors = _interceptors.slice();
 	if(options.interceptors) interceptors = interceptors.concat(options.interceptors);
-
 	interceptors.forEach((interceptorObj, index) => {
 		let interceptor = new interceptorObj({response});
 		return interceptor.response(response, raw);
 	});
-
 	return response;
 }
